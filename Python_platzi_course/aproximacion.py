d6a5f8d292fcb805117d5431b2d2caace0e3779c
Fernando Urruchurtu <furruchurto@uninorte.edu.co>
2021-03-27 17:48:53 -0500

    Aproximación número raíz cuadrada

---
+++ Python_platzi_course/aproximacion.py
@@ -0,0 +1,14 @@
+#No se puede ser más preciso y más rápido con los algoritmos
+objetivo = int(input("Escoge un número"))
+epsilon = 0.01 #Precisión
+paso = epsilon**2
+respuesta = 0.0
+
+while abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:
+    respuesta += paso
+    
+if abs(respuesta**2 - objetivo) >= epsilon:
+    print(f"No se encontró la raíz cuadrada de {objetivo}")
+else:
+    print(f"La raíz cuadrada de {objetivo} es {respuesta}")
+
