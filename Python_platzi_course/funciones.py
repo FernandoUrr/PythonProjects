867e290870975ca91a64082b28cb961d612f4d8c
Fernando Urruchurtu <furruchurto@uninorte.edu.co>
2021-03-27 19:05:08 -0500

    Excepciones y test de caja negra

--- Python_platzi_course/funciones.py
+++ Python_platzi_course/funciones.py
@@ -1,18 +1,31 @@
 import unittest
-#Pruebas
-def suma(num_1, num_2):
-    return num_1 + num_2
+#Pruebas de caja negra
+# def suma(num_1, num_2):
+#     return num_1 + num_2
 
 
 
-class CajaNegraTest(unittest.TestCase):
-    def test_suma_dos_positivos(self):
-        num_1 = 10
-        num_2 = 5
+# class CajaNegraTest(unittest.TestCase):
+#     def test_suma_dos_positivos(self):
+#         num_1 = 10
+#         num_2 = 5
         
-        resultado = suma(num_1, num_2)
-        self.assertEqual(resultado, 15)
+#         resultado = suma(num_1, num_2)
+#         self.assertEqual(resultado, 15)
 
-if __name__ == "__main__":
-    unittest.main()
+
+#Manejo de excepciones
+def divide_elementos_de_lista(lista, divisor):
+    try:
+        return [i/ divisor for i in lista]
+    except ZeroDivisionError as e:
+        return lista
+
+lista = list (range(10))
+divisor = 2
+
+print(divide_elementos_de_lista(lista, divisor))
+
+# if __name__ == "__main__":
+#     unittest.main()
 
