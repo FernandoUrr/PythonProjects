d80db2ea6c2eaa5fe4e56d8f42bae718310ad2e0
Fernando Urruchurtu <furruchurto@uninorte.edu.co>
2021-03-27 22:12:50 -0500

    Función compuesta

---
+++ Python_platzi_course/calculo_datascience3.py
@@ -0,0 +1,14 @@
+#Funciones dentro de otras funciones
+import numpy as np
+import matplotlib.pyplot as plt
+
+def g(x):
+    return x**2
+
+def f(x):
+    return np.sin(x)
+
+x = np.linspace(-10, 10 , num = 1000) #Genera un vector
+fog = f(g(x))
+plt.plot(x, fog)
+plt.show()
