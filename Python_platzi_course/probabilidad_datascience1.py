832e4887d270851fd948cf01b8832ca23ba422fb
Fernando Urruchurtu <furruchurto@uninorte.edu.co>
2021-04-01 16:39:08 -0500

    Distribución normal

---
+++ Python_platzi_course/probabilidad_datascience1.py
@@ -0,0 +1,27 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+from scipy.stats import norm
+
+#dist = norm(0, 1)
+# x = np.arange(-4, 4, 0.1) #Linspace no funciona con floats
+# y = [dist.pdf(value) for value in x]
+# plt.plot(x, y)
+# plt.show()
+
+#Panda sirve para leer exceles
+df = pd.read_excel("data_frames/s057.xls")
+arr = df["Normally Distributed Housefly Wing Lengths"].values[4:]
+# values, dist = np.unique(arr, return_counts = True)
+# plt.bar(values, dist)
+# plt.show()
+#Estimar la distribución
+mu = arr.mean() #Calcula el promedio
+sigma = arr.std() #Calcula la desviación
+dist = norm(mu, sigma)
+x = np.arange(30, 60, 0.1)
+y = [dist.pdf(value) for value in x]
+plt.plot(x, y)
+values, dist = np.unique(arr, return_counts = True)
+plt.bar(values, dist/len(arr))
+plt.show()
